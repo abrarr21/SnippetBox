4179719c9d4aaa318fc752a52a0201fb9c952b7d
Abrar <abrarrr2110@gmail.com>
2025-05-18 00:45:43 +0530

    routing refactor

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -36,23 +36,10 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	// Create a file server which serves the files out of the "./ui/static/" directory.
-	// Note that the path given to the http.Dir function is relative to the project's directory root.
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-
-	// Use the mux.Handle() function to register the file server as the handler
-	// all URL paths that start with /static/. For matching paths, we strip the "/static" prefix before the request reaches the server.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(), // mux -> app.routes()
 	}
 
 	// *addr dereferences the flag pointer to get the actual address string (e.g., ":6969")
