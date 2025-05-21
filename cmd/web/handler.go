3fa3b59b9285f7d82152818df9ad068b15acfb20
Abrar <abrarrr2110@gmail.com>
2025-05-21 17:59:24 +0530

    caching templates

--- cmd/web/handler.go
+++ cmd/web/handler.go
@@ -16,6 +16,14 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	s, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	// data := &templateData{Snippets: s}
+	//
 	// files := []string{
 	// 	"./ui/html/home.page.tmpl",
 	// 	"./ui/html/base.layout.tmpl",
@@ -24,28 +32,21 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	//
 	// ts, err := template.ParseFiles(files...)
 	// if err != nil {
-	// 	// app.errorLog.Println(err.Error())
-	// 	// http.Error(w, "Internal Server Error", 500)
-	// 	app.serverError(w, err) // using the serverError() helper function
+	// 	app.serverError(w, err)
 	// 	return
 	// }
 	//
-	// err = ts.Execute(w, nil)
+	// err = ts.Execute(w, data)
 	// if err != nil {
-	// 	// app.errorLog.Println(err.Error())
-	// 	// http.Error(w, "Internal Server Error", 500)
-	// 	app.serverError(w, err) //serverError()
+	// 	app.serverError(w, err)
+	// }
+	//
+	// for _, snippet := range s {
+	// 	fmt.Fprintf(w, "%v\n", snippet)
 	// }
 
-	s, err := app.snippets.Latest()
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-
-	for _, snippet := range s {
-		fmt.Fprintf(w, "%v\n", snippet)
-	}
+	//Use the new render helper to replace above commented code
+	app.render(w, r, "home.page.tmpl", &templateData{Snippets: s})
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
@@ -65,7 +66,33 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%v", s)
+	// data := &templateData{Snippet: s}
+	//
+	// files := []string{
+	// 	"./ui/html/show.page.tmpl",
+	// 	"./ui/html/base.layout.tmpl",
+	// 	"./ui/html/footer.partial.tmpl",
+	// }
+	//
+	// ts, err := template.ParseFiles(files...)
+	// if err != nil {
+	// 	// app.errorLog.Println(err.Error())
+	// 	// http.Error(w, "Internal Server Error", 500)
+	// 	app.serverError(w, err) // using the serverError() helper function
+	// 	return
+	// }
+	//
+	// err = ts.Execute(w, data)
+	// if err != nil {
+	// 	// app.errorLog.Println(err.Error())
+	// 	// http.Error(w, "Internal Server Error", 500)
+	// 	app.serverError(w, err) //serverError()
+	// }
+	//
+	// fmt.Fprintf(w, "%v", s)
+
+	// Use the render helper to replace above commented code
+	app.render(w, r, "show.page.tmpl", &templateData{Snippet: s})
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
